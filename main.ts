7afe13143a842520e30361167012e35ee7018a68
NovqiGarrix <novqigarrix@gmail.com>
2022-09-08 21:57:48 +0700

    First Commit

---
+++ main.ts
@@ -0,0 +1,35 @@
+import { configAsync } from '@deps';
+
+import createServer from '@app';
+import logger from '@utils/logger.ts';
+
+const env = await configAsync();
+const abortController = new AbortController();
+
+const DENO_ENV = Deno.env.get("DENO_ENV") || env.DENO_ENV;
+const IS_PROD = DENO_ENV === "prod";
+
+const PORT = +(Deno.env.get("PORT") || env.PORT) || 4000;
+
+const app = createServer();
+
+const signals = ["SIGINT", "SIGTERM"];
+for (let systemSignal of signals) {
+    if (Deno.build.os === "windows" && systemSignal === "SIGTERM") {
+        systemSignal = "SIGBREAK"
+    }
+    Deno.addSignalListener(systemSignal as Deno.Signal, () => {
+        logger.warning(`Received ${systemSignal}, exiting...`.toUpperCase());
+        Deno.exit(0);
+    })
+}
+
+globalThis.addEventListener("unload", () => {
+    abortController.abort();
+})
+
+app.addEventListener("listen", ({ hostname, port, serverType }) => {
+    logger.info(`Listening on ${hostname}:${port} with ${serverType} SERVER`.toUpperCase());
+})
+
+await app.listen({ port: PORT, hostname: IS_PROD ? undefined : "localhost", signal: abortController.signal });
---
+++ src/utils/logger.ts
@@ -0,0 +1,22 @@
+import { Format, Houston, ConsoleTransport, LogLevel, TextPrefix, Color, dayjs, FileTransport } from '@deps';
+
+const logger = new Houston([
+    new ConsoleTransport([LogLevel["Error"], LogLevel["Success"], LogLevel["Warning"], LogLevel["Info"]], {
+        format: Format["json"],
+        prefix: new TextPrefix(dayjs().format("MMMM D, YYYY h:mm:ss A")),
+        logColors: {
+            [LogLevel.Info]: Color.Black,
+            [LogLevel.Error]: Color.Red,
+            [LogLevel.Success]: Color.Green,
+            [LogLevel.Warning]: Color.Yellow
+        }
+    }),
+
+    // Deno Deploy does not allow you to write files.
+    // new FileTransport("./logs", [LogLevel["Error"]], {
+    //     format: Format["text"],
+    //     prefix: new TextPrefix(dayjs().format("MMMM D, YYYY h:mm:ss A")),
+    // })
+])
+
+export default logger;
